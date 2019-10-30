9adb06693f55288727a7c181157a1f77d7dbdd10
lbb1811 <liubin_2014@outlook.com>
2019-10-30 15:49:31 +0800

    Revert "6-04 使用Redux-thunk中间件实现ajax数据请求"
    This reverts commit deb6efd3a38384d419619537c8f266db483caf2d.

--- src/store/index.js
+++ src/store/index.js
@@ -1,28 +1,9 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore } from 'redux';
 import reducer from './reducer';
-import thunk from 'redux-thunk';
 
-// const store = createStore(
-//   reducer,
-//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-// );
-
-/**
- * redux-thunk中间件引入 与 redux引入 (github: redux-devtools-extension)
- * redux-thunk 使 actionCreator 函数的返回值可以是一个函数。
- */
-
-const composeEnhancers =
-  typeof window === 'object' &&
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
-    }) : compose;
-
-const enhancer = composeEnhancers(
-  applyMiddleware(thunk),
-  // other store enhancers if any
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
-const store = createStore(reducer, enhancer);
 
 export default store;
